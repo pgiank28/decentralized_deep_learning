33bb226c90f343f3fe0881112e3f23dcf208e9f4
pgiank28 <pgiankoulidis@outlook.com>
2019-03-14 16:04:36 +0200

    context configuration

--- context.py
+++ context.py
@@ -1,4 +1,7 @@
 from __future__ import print_function
+import os
+import sys
+import shutil
 
 class SparkContext(object):
     def __init__(self):
@@ -7,3 +10,13 @@ class SparkContext(object):
     def run(self):
         self.master = 1
 
+
+class sparkGet(SparkContext):
+    def __init__(self,conf):
+        self.status = 0
+    
+    def conf(self):
+        self.status = 1
+
+print("hi")
+g = sparkGet(2)
