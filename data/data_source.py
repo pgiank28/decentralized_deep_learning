8a7efeae58ab86a28b689d9cbea538b52afb4ced
pgiank28 <pgiankoulidis@outlook.com>
2019-03-18 15:23:12 +0200

    core implementation

--- data/data_source.py
+++ data/data_source.py
@@ -5,3 +5,11 @@ from pyspark.sql.types import StructType, StructField, FloatType
 import pandas as pd
 import numpy as np
 import configparser
+
+class DataSource:
+    def __init__(self):
+       
+        conf = SparkConf().setAppName("DDLSpark").setMaster("spark://localhost:7077")
+        self.sc = SparkContext(conf=conf)
+        self.spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
