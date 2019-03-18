db57d278f7b822dc251fd7a6b5271b35091aa98a
pgiank28 <pgiankoulidis@outlook.com>
2019-03-18 11:50:54 +0200

    data dependencies

---
+++ data/data_source.py
@@ -0,0 +1,7 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, FloatType
+
+import pandas as pd
+import numpy as np
+import configparser
