de3a33a5af6819c7a21d15cc6077ddc1238d7d03
pgiank28 <pgiankoulidis@outlook.com>
2019-03-25 14:15:43 +0200

    Create core jar

--- examples/basic_examples2/src/main/java/ddlspark/examples/App.java
+++ examples/basic_examples2/src/main/java/ddlspark/examples/App.java
@@ -1,17 +1,19 @@
 package ddlspark.examples;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
+import core.NN_Estimator;
+
 public class App 
 {
     public static void main( String[] args )
     {
         SparkSession spark = SparkSession
       .builder()
-      .appName("DDLSpark")
+      .appName("JavaSparkPi")
       .getOrCreate();
 
-      JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
-      System.out.println("I made it!!!");
-      spark.stop();
+    JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+    System.out.println("I made it!!")
+    spark.stop();
     }
 }
