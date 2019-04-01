82a9b84cd5bfd0d463e5b2eb05231f944a63a141
pgiank28 <pgiankoulidis@outlook.com>
2019-04-01 23:43:24 +0300

    create local tensorflow model

--- core/src/main/java/ddlspark/core/cite.java
+++ core/src/main/java/ddlspark/core/cite.java
@@ -1,6 +1,16 @@
 package ddlspark.core;
 import org.apache.spark.sql.Dataset;
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
 
 public class cite{
-
+      Tensor state;
+      public static void createTfCluster(Graph g)throws Exception{
+        
+          try(Session s = new Session(g)){
+            this.state = s.runner.fetch("weights").run();
+          }
+        
 }
