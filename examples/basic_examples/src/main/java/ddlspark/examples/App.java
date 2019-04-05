564c6d0a74f47b828ebd43b43ad451d52ae15bbc
pgiank28 <pgiankoulidis@outlook.com>
2019-04-05 14:34:09 +0300

    tf example

--- examples/basic_examples/src/main/java/ddlspark/examples/App.java
+++ examples/basic_examples/src/main/java/ddlspark/examples/App.java
@@ -1,13 +1,20 @@
 package ddlspark.examples;
 
-/**
- * Hello world!
- *
- */
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+
 public class App 
 {
     public static void main( String[] args )
     {
+	SparkConf conf = new SparkConf().setAppName("master-slave example").setMaster("local");
+	JavaSparkContext sc = new JavaSparkContext(conf);
         System.out.println( "Hello World!" );
+	JavaRDD<String> distFile = sc.textFile("file:///home/pantelisg/Desktop/data.txt");
+	distFile.map(s->s.length()).reduce((a,b)->a+b);
+	sc.stop();
     }
 }
---
+++ examples/basic_examples3/src/main/java/ddlspark/examples/simple.java
@@ -0,0 +1,29 @@
+package ddlspark.examples;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+public class simple{
+	public static void main( String[] args )
+    {
+	SparkConf conf = new SparkConf().setAppName("master-slave example").setMaster("local[2]");
+	//JavaSparkContext sc = new JavaSparkContext(conf);
+	
+	SparkSession session = SparkSession.builder().getOrCreate();
+        System.out.println( "Hello World!" );
+	//JavaRDD<String> distFile = sc.textFile("file:///home/pantelisg/Desktop/data.txt");
+	Dataset<Row> df = session.read().csv("file:///home/pantelisg/Desktop/train.csv");
+	
+        df.show(13);
+        
+        
+	session.stop();
+    }
+
+
+}
