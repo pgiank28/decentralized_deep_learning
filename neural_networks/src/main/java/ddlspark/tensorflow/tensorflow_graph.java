134a8367cf4c423d0c50a94546f18ca784b9d3c9
pgiank28 <pgiankoulidis@outlook.com>
2019-04-09 11:45:40 +0300

    resolving dependencies

--- neural_networks/src/main/java/ddlspark/tensorflow/tensorflow_graph.java
+++ neural_networks/src/main/java/ddlspark/tensorflow/tensorflow_graph.java
@@ -1,5 +1,7 @@
 package ddlspark.tensorflow;
 import java.io.Serializable;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
 import org.tensorflow.Graph;
 
 public class tensorflow_graph implements Serializable{
