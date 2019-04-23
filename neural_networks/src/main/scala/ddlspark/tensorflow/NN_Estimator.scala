88b6b9275b39049e631f868b2fda802157949b74
pgiank28 <pgiankoulidis@outlook.com>
2019-04-23 16:20:10 +0300

    compiling scala

---
+++ neural_networks/src/main/scala/ddlspark/tensorflow/NN_Estimator.scala
@@ -0,0 +1,12 @@
+package ddlspark.tensorflow
+
+import org.apache.spark.sql.Dataset
+
+class NN_Estimator{
+
+	
+
+	private def train(dataset:Dataset[_]):{
+
+	}	
+}
---
+++ neural_networks/src/main/scala/ddlspark/tensorflow/ScalaRunner.scala
@@ -0,0 +1,7 @@
+package ddlspark.tensorflow
+
+object ScalaRunner {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
