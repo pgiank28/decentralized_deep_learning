8b0bd65c49859c729e6a1f222977b051cd18e218
pgiank28 <pgiankoulidis@outlook.com>
2019-06-06 16:13:46 +0300

    graph training new experiments

--- examples/examples_01/src/main/scala/ddlspark/examples/GraphTrainingExample.scala
+++ examples/examples_01/src/main/scala/ddlspark/examples/GraphTrainingExample.scala
@@ -3,13 +3,37 @@ package ddlspark.examples
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 
+import ddlspark.core.model
+import ddlspark.core.localModel
+import ddlspark.core.activationFunctions
+
 
 object GraphTrainingExample{
 
 	def main(args:Array[String]){
+		val path = "/home/pantelisg/Desktop/train.csv"
 		val conf:SparkConf = new SparkConf().setAppName("Naive parallel training")
 		val sc:SparkContext = new SparkContext(conf)
+		val rdd = sc.textFile(path)
 
+		/** Create model description 
+		  * Implemented in the filesystem (neural_networks) with the help
+		  * of popular deep learning frameworks like Tensorflow
+		  * 
+		  * We take an untrained neural network as input and convert it to
+		  * an instance of our local model framework (in core) 
+		**/
+
+		var y = "Reference Model"
+		val model = new model(inLayer,hLayer,ouLayer,layers,sSize,acFunction) //General neural network topology
+ 		val lModel = new localModel(model,x,0,losFunction,0.01,bSize,geometry,sofInput) //Local instance of this topology
+
+		rdd.foreach{x => {y = this.agg(x,y) } }//Must be a singleton object visible(here 'GraphTrainingExample')
 		sc.stop()	
+	}
+
+	def agg(s:String,state:localModel):localModel = {
+		println("Hi")
+		"Hi"
 	}
 }
